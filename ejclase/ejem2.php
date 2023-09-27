51b99f3c03882a6835af629795b8368a3c712da8
Christian Tomás <tomascabrera21@iessantiagohernandez.com>
2023-09-27 16:51:19 +0200

    Ejemplo2

---
+++ ejclase/ejem2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+        <?php
+            $Variable = "Hola, soy un script de PHP!";
+            echo $Variable;
+        ?>
+    </body>
+</html>
