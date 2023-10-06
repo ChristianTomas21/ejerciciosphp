a50d2965d72821fb3f27f7005934912a581e30e5
Christian Tomás <tomascabrera21@iessantiagohernandez.com>
2023-10-06 19:07:05 +0200

    pasar a una nueva carpeta los ejercicios y seguir con formularios subiendo archivo

--- ejclase/encuesta.php
+++ ejclase/encuesta.php
@@ -1,25 +1,32 @@
 <?php
-    echo "<h2>Resultado encuestas</h2>";
-    if(isset($_POST["ienviar"]) && !empty($_POST["ienviar"])){
+echo "<h2>Resultado encuestas</h2>";
+if (isset($_POST["ienviar"]) && !empty($_POST["ienviar"])) {
 
-        //chequeo del radio button
-        if(isset($_POST["lprog"]) && !empty($_POST["lprog"])){
-            $lenguaje = $_POST["lprog"];
-            echo "<h4>Tu lenguaje preferido es: $lenguaje</h4>";
+    //chequeo del radio button
+    if (isset($_POST["lprog"]) && !empty($_POST["lprog"])) {
+        $lenguaje = $_POST["lprog"];
+        echo "<h4>Tu lenguaje preferido es: $lenguaje</h4>";
+    }
+    //chequeo del checkbox
+    //echo "<br>Num elemento : " . count($_POST["ntrans"]);
+    if (isset($_POST["ntrans"]) && !empty($_POST["ntrans"])) {
+        $transportes = $_POST["ntrans"];
+        foreach ($transportes as $transporte) {
+            echo "<br>Transporte seleccionado : " . $transporte;
         }
-         //chequeo del checkbox
-         //echo "<br>Num elemento : " . count($_POST["ntrans"]);
-        if(isset($_POST["ntrans"]) && !empty($_POST["ntrans"])){
-            $transportes = $_POST["ntrans"];
-            foreach($transportes as $transporte){
-                echo "<br>Transporte seleccionado : ". $transporte;
-            }
-        }else{
-            echo "<br>No has seleccionado ningún transporte";
-        }
-         //chequeo del lista seleccion simple
-         if(isset($_POST["sfutbol"]) && !empty($_POST["sfutbol"])){
-            $efutbol = $_POST["sfutbol"];
-            echo "<h4>Tu equipo preferido es: $efutbol</h4>";
+    } else {
+        echo "<br>No has seleccionado ningún transporte";
+    }
+    //chequeo del lista seleccion simple
+    if (isset($_POST["sfutbol"]) && !empty($_POST["sfutbol"])) {
+        $efutbol = $_POST["sfutbol"];
+        echo "<h4>Tu equipo preferido es: $efutbol</h4>";
+    }
+    //Lista multiple de menu
+    if (isset($_POST["smenu"]) && !empty($_POST["smenu"])) {
+        $menus = $_POST["smenu"];
+        foreach ($menus as $plato) {
+            echo "<br>Palto preferido : " . $plato;
         }
     }
+}
